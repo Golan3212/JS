8da08846cfd14fd81f6d3db7ea144c549d85314c
Golan3212 <93038099+Golan3212@users.noreply.github.com>
2021-12-05 17:57:02 +0700

    Add files via upload

---
+++ homework-2/2.4.js
@@ -0,0 +1,20 @@
+'use strict'
+function summ (h, v) {
+    return h + v;
+}
+ console.log(summ(3, 5));
+
+ function multiply(h, v) {
+     return h * v;
+ }
+ console.log(multiply(3, 5));
+
+ function divide(h, v) {
+    return h / v;
+}
+console.log(divide(3, 5));
+
+function subtract(h, v) {
+    return h - v;
+}
+console.log(subtract(3, 5));
