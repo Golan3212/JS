d7fe89f822e5d41e0f38a3561a6f0140033a3a69
Golan3212 <93038099+Golan3212@users.noreply.github.com>
2021-12-10 21:54:54 +0700

    Add files via upload

---
+++ homework-3/3.1.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+
+    for ( let i = 0; i <= 10; i++) {
+        if (i == 0) { 
+            console.log( +i +" eto zero");
+           i++;
+        }
+        if (i % 2){
+            console.log (+i +" eto 4islo ne4etnoe");
+           
+        }
+        else {
+            console.log( +i +" eto 4islo 4etnoe");
+        }
+        
+    }   
+
+   
---
+++ homework-3/3.6.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let x = "x"
+    for( x; x.length <= 20; x += "x"){
+       
+        console.log(x);
+
+     }
+     
